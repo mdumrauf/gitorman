f3d0589e69fe23021d4b37940169e47e6d902743
Matías Dumrauf <matias.dumrauf@gmail.com>
2012-04-25 02:40:18 -0300

    Closes #4 - Updated script to check for existing repos.

--- src/organization-run.rb
+++ src/organization-run.rb
@@ -5,6 +5,8 @@ require 'logger'
 
 $logger = Logger.new("github-manager.log")
 
+$logger.info "GitorMan initialized!"
+
 begin
 	groups = MysqlSisopHelper::confirmed_groups("xxx.xxx.x.xxx", "xxxxxx", "xxxxxx", "xxxx")
 
@@ -14,12 +16,22 @@ rescue Mysql::Error => e
   $logger.error "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
 end
 
+$logger.info "#{groups.length} groups retrieved from database"
+
 client = GithubManager.new("sisoputnfrba", "mdumrauf", "*************")
 
 id_team_ayudantes = "xxxxxxx"
 number_of_repos = 0
 
-groups.each{ |group, users|
+existing_repos = client.list_repos("private").map { |repo| repo.name }
+
+$logger.info "#{existing_repos.length} private repos found in Organization: sisoputnfrba"
+
+new_groups = groups.select { |group|
+  !existing_repos.member? "2012-1c-#{group.downcase.strip.gsub(" ", "-")}"
+}
+
+new_groups.each{ |group, users|
 
   repo_name = "2012-1c-#{group.downcase.strip.gsub(" ", "-")}"
   team_name = group
@@ -31,11 +43,14 @@ groups.each{ |group, users|
     client.initialize_repo(team_name, repo_name)
 
     client.add_team_to_repo(id_team_ayudantes, repo_name)
-  rescue
-    $logger.error "Could not create repo or team #{repo_name}"
+  rescue Exception => e
+    $logger.error "Could not create repo or team"
+    $logger.error e.message
+    $logger.error e.backtrace.inspect
   end
+
   $logger.info "Created repo: #{repo_name} for team: #{team_name}"
-  number_of_repos++
+  number_of_repos += 1
 }
 
-$logger.info "#{number_of_repos}/#{groups.length} repos and teams created"
+$logger.info "#{number_of_repos} repos and teams were created"
